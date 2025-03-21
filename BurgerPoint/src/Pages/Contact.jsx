c4fb42cc54c6587dad9e53336864ba894968f60f
BhatiRishabh <rishabhbhati831@gmail.com>
2025-03-22 00:38:33 +0530

    Enhance Contact page with a header prompting user engagement

--- BurgerPoint/src/Pages/Contact.jsx
+++ BurgerPoint/src/Pages/Contact.jsx
@@ -3,7 +3,16 @@ import EmailForm from '../Components/EmailForm/ Emailform'
 
 function Contact() {
   return (
-    <div className='p-4'><EmailForm/></div>
+    <div className='p-4'>
+      <div class="flex items-center justify-center p-4 ">
+      <h1 class=" items-center text-5xl font-bold text-transparent bg-clip-text dark:bg-gradient-to-r from-indigo-500 to-purple-600 
+              hover:tracking-widest transition-all duration-300 ease-in-out drop-shadow-lg">
+    I've heard networking is good, let us connect?
+  </h1>
+  </div>
+      <EmailForm/>
+      
+      </div>
   )
 }
 
